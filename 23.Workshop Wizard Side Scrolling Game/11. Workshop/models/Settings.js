a6fd0dc6926617e7c448d6a427bd834d164daa2d
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-07-10 22:12:14 +0300

    Delete 23.Workshop Wizard Side Scrolling Game/11. Workshop directory

--- 23.Workshop Wizard Side Scrolling Game/11. Workshop/models/Settings.js
+++
@@ -1,9 +0,0 @@
-export default class Settings {
-    constructor(){
-        this.destructablesMinSize = 40;
-        this.destructablesMaxSize = 100;
-        this.width = 800;
-        this.height = 480;
-        this.spawnInterval = 2000;
-    }
-}
